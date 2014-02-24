0fa0dc727000d856bc1f4d35d238ce92bdbe4f78
Ahmet Besir Kurtulmus <besirkurtulmus@gmail.com>
2014-02-24 11:21:56 +0200

    Add RandomList() function

--- Sorting/MergeSort.py
+++ Sorting/MergeSort.py
@@ -21,6 +21,8 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
+from random import randint
+
 def Merge(list1, list2):
 	"""
 	Description: Merges two sorted lists.
@@ -63,3 +65,21 @@ def MergeSort(l):
 		return Merge(MergeSort(firstHalf), MergeSort(secondHalf))
 	else:
 		return l
+
+def RandomList(length, min, max):
+	"""
+	Description: Generates a list with the given length.
+
+	Args:
+		length (int Type): The length of the randomly generated list.
+		min (int type): The minimum possible number.
+		max (int type): The maximum possible number.
+
+	Examples:
+	>>> RandomList(5)
+	[3, 4, 8, 1, 6]
+	"""
+	l = []
+	for i in range(length):
+		l.append(randint(min, max))
+	return l
